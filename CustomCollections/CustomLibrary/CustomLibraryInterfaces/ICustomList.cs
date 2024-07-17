368304f2f1d54c4d2fba7e9b0bd1d16bde710bf0
andriidome <onewinged.click@gmail.com>
2024-07-17 18:23:39 +0300

    Update ICustomList.cs

--- CustomCollections/CustomLibrary/CustomLibraryInterfaces/ICustomList.cs
+++ CustomCollections/CustomLibrary/CustomLibraryInterfaces/ICustomList.cs
@@ -1,8 +1,24 @@
-﻿using System;
-namespace CustomLibrary
+using System;
+namespace CustomLibraryInterfaces
 {
-	public interface EmptyInterface
-	{
-	}
+    public interface ICustomList<T>: ICustomCollection<T>
+    {
+        public void Insert(T obj, int index);
+
+        public void Remove(T obj);
+
+        public void RemoveAt(int index);
+
+        public void Clear();
+
+        public int IndexOf(T obj);
+
+        public void Reverse();
+
+        public T? this[int index]
+        {
+            get; set;
+        }
+    }
 }
 
